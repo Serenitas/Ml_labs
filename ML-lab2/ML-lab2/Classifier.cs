56f78393efd3d31c5a59bf3776b5efd7c218f3a4
Svetlana Strekalova <svetlana.strekalova@gmail.com>
2016-12-05 00:40:01 +0700

    Added second lab

---
+++ ML-lab2/ML-lab2/Classifier.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+
+namespace ML_lab2
+{
+    class Classifier
+    {
+        private static readonly string _filepath = "D:\\std\\Бигдата Павловский\\ML-lab2\\ML-lab2\\Iris.csv";
+        private Dictionary<double[], bool> train = new Dictionary<double[], bool>();
+        private Dictionary<double[], bool> test = new Dictionary<double[], bool>();
+
+        private const int _trainCount = 20;
+        private const int _classElementCount = 50;
+        private const int _iterationCount = 10000;
+
+        private const double _alpha = 0.01;
+        private double[] thetas = new double[4];
+
+        private void ReadData()
+        {
+            var count = 0;
+            string str;
+            bool isFirstClass = true;
+            using (var stream = new StreamReader(_filepath))
+            {
+                while ((str = stream.ReadLine()) != null)
+                {
+                    var elements = str.Split(',');
+                    var features = new double[4];
+                    for (int i = 0; i < 4; i++)
+                    {
+                        features[i] = double.Parse(elements[i], System.Globalization.CultureInfo.InvariantCulture);
+                    }
+                    if (count < _trainCount)
+                        train.Add(features, isFirstClass);
+                    else
+                        test.Add(features, isFirstClass);
+                    count++;
+                    if (count == _classElementCount)
+                    {
+                        count = 0;
+                        isFirstClass = false;
+                    }
+                }
+            }
+        }
+
+        private double LogisticCurve(double z)
+        {
+            return 1.0 / (1.0 + Math.Exp(-z));
+        }
+
+        private double ScalarMultiply(double[] v1, double[] v2)
+        {
+            var res = 0.0;
+            for (var i = 0; i < v1.Count(); i++)
+                res += v1[i] * v2[i];
+            return res;
+        } 
+
+        private void TrainRegression()
+        {
+            for (var i = 0; i < _iterationCount; i++)
+            {
+                double[] currThetas = new double[4];
+                Array.Copy(thetas, currThetas, thetas.Count());
+                for (var j = 0; j < 4; j++)
+                {
+                    double sum = 0.0;
+                    for (var k = 0; k < train.Count; k++)
+                    {
+                        var iris = train.ElementAt(k);
+                        var rightAns = (iris.Value) ? 1.0 : 0.0;
+                        sum += (rightAns - LogisticCurve(ScalarMultiply(currThetas, iris.Key))) * iris.Key.ElementAt(j);
+                    }
+                    sum /= train.Count;
+                    sum *= _alpha;
+                    thetas[j] = currThetas[j] + sum;
+                }
+            }
+        }
+
+        public LinkedList<Point> Classify()
+        {
+            ReadData();
+            TrainRegression();
+
+            var result = new LinkedList<Point>();
+
+            for (var threshhold = 0.0; threshhold <= 1.0; threshhold += 0.0001)
+            {
+                var testSize = (_classElementCount - _trainCount) * 3;
+                double FPR = 0.0, TPR = 0.0;
+                for (var j = 0; j < testSize; j++)
+                {
+                    var currAns = LogisticCurve(ScalarMultiply(thetas, test.ElementAt(j).Key));
+                    if (currAns >= threshhold)
+                        if (test.ElementAt(j).Value)
+                            TPR++;
+                        else
+                            FPR++;
+                }
+                double p = _classElementCount - _trainCount;
+                double n = p * 2;
+                TPR /= p;
+                FPR /= n;
+                result.AddLast(new Point(FPR, TPR));
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ML-lab2/ML-lab2/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ML_lab2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            drawGraph(new Classifier().Classify());
+        }
+
+        public double getAuc(LinkedList<Point> points)
+        {
+            var result = 0.0;
+            for (var i = 0; i < points.Count - 1; i++)
+            {
+                var curr = points.ElementAt(i);
+                var next = points.ElementAt(i + 1);
+
+                result += Math.Abs(curr.X - next.X) * (curr.Y + next.Y) * 0.5;
+            }
+            return result;
+        }
+
+        public void setText(string text)
+        {
+            this.text.AppendText(text);
+        }
+
+        public void drawGraph(LinkedList<Point> points)
+        {
+            var arrow = new Line();
+            arrow.Stroke = Brushes.Gray;
+            arrow.X1 = 40;
+            arrow.Y1 = 20;
+            arrow.X2 = 40;
+            arrow.Y2 = 1 * (image.Height - 80) + 40;
+            image.Children.Add(arrow);
+            var arrow1 = new Line();
+            arrow1.Stroke = Brushes.Gray;
+            arrow1.X1 = 1 * (image.Width - 80) + 60;
+            arrow1.Y1 = 1 * (image.Height - 80) + 40;
+            arrow1.X2 = 40;
+            arrow1.Y2 = 1 * (image.Height - 80) + 40;
+            image.Children.Add(arrow1);
+
+            for (int i = 0; i < points.Count - 1; i++)
+            {
+                var line = new Line();
+                line.Stroke = Brushes.CadetBlue;
+                //line.Fill = Brushes.Magenta;
+                line.StrokeThickness = 3;
+                line.X1 = points.ElementAt(i).X * (image.Width - 80) + 40;
+                line.Y1 = (1 - points.ElementAt(i).Y) * (image.Height - 80) + 40;
+                line.X2 = points.ElementAt(i + 1).X * (image.Width - 80) + 40;
+                line.Y2 = (1 - points.ElementAt(i + 1).Y) * (image.Height - 80) + 40;
+                image.Children.Add(line);
+            }
+            setText(string.Concat(getAuc(points)));
+        }
+    }
+}
